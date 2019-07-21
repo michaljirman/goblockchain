e9f62292dfb2c67169269608858c57dd4717c59d
Michal Jirman <jirman.michal@gmail.com>
2019-07-21 20:05:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### goblockchain
+
+Basic implementation of the blockchain / cryptocurrency in the Golang (WIP)
+
+#### References
+
+Based on the Tensor's [tutorial](https://github.com/tensor-programming/golang-blockchain)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/michaljirman/goblockchain/blockchain"
+)
+
+func main() {
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+
+}
